57053575a027b9a0444c591426c42a9fcf9cba8d
Davide Bettio <davide.bettio@ispirata.com>
2019-03-05 17:05:02 +0100

    First commit
    hmm.jpg

---
+++ lib/astarte_streams.ex
@@ -0,0 +1,18 @@
+defmodule Astarte.Streams do
+  @moduledoc """
+  Documentation for Astarte.Streams.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Astarte.Streams.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/astarte_streams_test.exs
@@ -0,0 +1,8 @@
+defmodule Astarte.StreamsTest do
+  use ExUnit.Case
+  doctest Astarte.Streams
+
+  test "greets the world" do
+    assert Astarte.Streams.hello() == :world
+  end
+end
