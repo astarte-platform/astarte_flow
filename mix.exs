af846bf1a85445126c0847fda61388c7ed0634f2
Davide Bettio <davide.bettio@ispirata.com>
2019-03-05 17:21:13 +0100

    Add .gitlab-ci.yml and run excoveralls
    Add minimal .gitlab-ci.yml which uses mix coveralls.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Astarte.Streams.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -22,8 +29,7 @@ defmodule Astarte.Streams.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
