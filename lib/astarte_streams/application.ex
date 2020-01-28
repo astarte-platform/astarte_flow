832ea6fea99d36af6da846ec69b75a11a8344ee1
Riccardo Binetti <riccardo.binetti@ispirata.com>
2020-01-28 18:46:21 +0100

    application: start Flows Registries and DynamicSupervisor

--- lib/astarte_streams/application.ex
+++ lib/astarte_streams/application.ex
@@ -26,6 +26,9 @@ defmodule Astarte.Streams.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
+      {Registry, keys: :unique, name: Astarte.Streams.Flows.Registry},
+      {Registry, keys: :duplicate, name: Astarte.Streams.Flows.RealmRegistry},
+      {DynamicSupervisor, strategy: :one_for_one, name: Astarte.Streams.Flows.Supervisor},
       Astarte.StreamsWeb.Endpoint
     ]
 
